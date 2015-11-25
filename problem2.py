e0ba4dd683d7a7edb527e110d6eb0b116dfa7156
Alexa Villaume <alexa@gin.local>
2015-11-24 21:14:57 -0800

    cleaning up function and variable names

--- problem2.py
+++ problem2.py
@@ -5,30 +5,30 @@ import matplotlib.pyplot as plt
 def gaussian(r):
     return (1/np.sqrt(2*np.pi))*np.exp(-r/2)
 
-def solver_FE(I, radius, Nr, constant, timescale):
-    # Mesh points in space
-    r, dr = np.linspace(0, radius, Nr+1, retstep=True)
-    # Mesh points in time
+def solve(initial_conds, radius, num_r, constant, timescale):
+    # Grid points in space
+    r, dr = np.linspace(0, radius, num_r+1, retstep=True)
+    # Grid points in time
     dt = 0.5
     Nt = int(round(timescale/float(dt)))
     t = np.linspace(0, timescale, Nt+1)
 
-    surfd = np.zeros(Nr+1)
-    surfd_1 = np.zeros(Nr+1)
+    surfd = np.zeros(num_r+1)
+    surfd_1 = np.zeros(num_r+1)
 
     # Set up initial condition
-    for i in range(0, Nr+1):
-        surfd_1[i] = I(r[i])
+    for i in range(0, num_r+1):
+        surfd_1[i] = initial_conds(r[i])
 
     for j in range(0, Nt):
-        for n in range(1, Nr):
-            surfd[n] = surfd_1[n] + (constant*dt/r[n])*(((r[n+1]**0.5 - \
+        for n in range(1, num_r):
+            surfd[n] = surfd_1[n] + (3*constant*dt/r[n])*(((r[n+1]**0.5 - \
             r[n]**0.5)/dr) *(surfd_1[n+1]*r[n+1]**2 - 2*(surfd_1[n]*r[n]**1) \
             + surfd_1[n-1]*r[n-1]**2))
 
     # Insert boundary conditions
     surfd[0] = 0.087
-    surfd[Nr] = 0
+    surfd[num_r] = 0
     # Switch variables before next step
     surfd_1, surfd = surfd, surfd_1
 
@@ -36,12 +36,14 @@ def solver_FE(I, radius, Nr, constant, timescale):
 
 if __name__ == '__main__':
 
+    alpha = 10e-2
+    sound_speed = 0.6
     radius = 3.4
-    Nr = 100
-    constant = 0.3
+    num_r = 100
+    constant = 0.1
     timescale = 1e3
 
-    surfd, r, t = solver_FE(gaussian, radius, Nr, constant, timescale)
+    surfd, r, t = solve(gaussian, radius, num_r, constant, timescale)
     print len(surfd), len(r), len(t)
 
     plt.plot(r, surfd, lw=2)
